7ba8ab371518f0a8501f47bf38941db8d879046b
Fabio Crusco da Silva <fcrusco77@gmail.com>
2026-03-10 18:50:37 -0300

    Add project files.

---
+++ AppPOO/Program.cs
@@ -0,0 +1,76 @@
+﻿//Encapsulamento - Alterar propriedade para private set { saldo = value; }
+using AppPOO.model;
+
+Console.WriteLine("Entre com o nome:");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Entre com o saldo em conta:");
+string input = Console.ReadLine();
+
+if (!decimal.TryParse(input, out decimal saldo))
+    Console.WriteLine("Valor inválido. Digite um número decimal válido.");
+
+//adicionar bloco try/catch no momento de instanciar a classe
+try
+{
+    ContaBancaria conta = new ContaBancaria(nome, saldo);
+    conta.ExibirSaldo();
+    //conta.Saldo = -500; //forçar alteração
+    string resposta;
+    do
+    {
+        Console.WriteLine("\nQual operação deseja realizar?");
+        Console.WriteLine("1 - Depósito");
+        Console.WriteLine("2 - Exibir Saldo");
+        Console.WriteLine("3 - Saque");
+
+        if (byte.TryParse(Console.ReadLine(), out byte opcao))
+        {
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Digite o valor para depósito:");
+                    input = Console.ReadLine();
+                    if (decimal.TryParse(input, out decimal valorDep) && valorDep > 0)
+                    {
+                        conta.Depositar(valorDep);
+                        conta.ExibirSaldo();
+                    }
+                    else
+                        Console.WriteLine("Valor inválido");
+
+                    break;
+                case 2:
+                    conta.ExibirSaldo();
+                    break;
+                case 3:
+                    Console.WriteLine("Digite o valor para saque:");
+                    input = Console.ReadLine();
+                    if (decimal.TryParse(input, out decimal valorSaque) && valorSaque > 0)
+                    {
+                        conta.Sacar(valorSaque);
+                        conta.ExibirSaldo();
+                    }
+                    else
+                        Console.WriteLine("Valor inválido");
+
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+        else
+            Console.WriteLine("Valor inválido");
+
+        Console.WriteLine("\nDeseja realizaar outra operação (S/N)?");
+        resposta = Console.ReadLine().Trim().ToUpper();
+
+    } while (resposta == "S");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
---
+++ AppPOO/model/ContaBancaria.cs
@@ -0,0 +1,67 @@
+﻿namespace AppPOO.model
+{
+    public class ContaBancaria
+    {
+
+        private string titular;
+        private decimal saldo;
+        private const decimal TARIFA_SAQUE = 2.50m;
+
+        //propriedade
+        public string Titular
+        {
+            get { return titular; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("O nome não pode ser vazio");
+
+                titular = value;
+            }
+        }
+        private decimal Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+        //construtor
+        public ContaBancaria(string titular, decimal saldoInicial)
+        {
+            Titular = titular;
+            Saldo = saldoInicial;
+        }
+
+        //método
+        public void Depositar(decimal valor)
+        {
+            Saldo += valor;
+            Console.WriteLine("Depósito realizado com sucesso!");
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"\nTitular: {Titular} - Saldo: R${Saldo}");
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("Saque inválido. Valor deve ser maior que zero.");
+                return;
+            }
+
+            decimal valorTotal = valor + TARIFA_SAQUE;
+
+            if (valorTotal <= Saldo)
+            {
+                Saldo -= valorTotal;
+                Console.WriteLine($"Saque de R${valorTotal} realizado com tarifa de {TARIFA_SAQUE}.");
+                Console.WriteLine($"Saldo atual R${saldo}.");
+            }
+            else
+                Console.WriteLine("Saldo insuficiente para saque.");
+        }
+    }
+}
